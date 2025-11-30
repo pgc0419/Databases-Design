7d120533809896465d8e8b1f16a97273e6c9061a
박근채 <pgc0419@gmail.com>
2025-12-01 04:46:07 +0900

    write select.js

--- src/routes/select.js
+++ src/routes/select.js
@@ -1,42 +1,50 @@
 import express from "express";
-import { selectSql, createSql } from "../database/sql";
+import { selectSql} from "../database/sql";
 
 const router = express.Router();
 
-router.get('/', async function (req, res) {
-    // TODO
-    const studentId = req.cookies.user;
-    const classes = await selectSql.getClasses(studentId);
-    const allClass = await selectSql.getAllClasses(studentId);
-    if (req.cookies.user) {
+router.get('/admin', async function (req, res) {
+    const userEmail = req.cookies.user_email;
+    const airport = await selectSql.getAirport();
+    const airplane = await selectSql.getAirplane();
+    const flight = await selectSql.getFlight();
+    const flightLeg = await selectSql.getFlightLeg(flight_number);
+    const legInstance = await selectSql.getLegInstance(leg_number, airplane_id);
+    
+    if (userEmail) {
         res.render('select', { 
-            user: req.cookies.user,
-            title: "classes",
-            title2: "allClass",
-            classes, 
-            allClass
+            user: req.cookies.user_email,
+            title: "airport",
+            title2: "airplane",
+            title3: "flight",
+            title4: "flightLeg",
+            title5: "legInstance",
+            airport, 
+            airplane,
+            flight,
+            flightLeg,
+            legInstance
         });
     } else {
-        res.render('/')
+        res.render('/admin')
     }
-
 });
 
-router.post('/', async(req, res) => {
-    // TODO
-    const data = {
-        cId: req.body.applyBtn,
-        sId: req.cookies.user,
-    };
-    if (await selectSql.checkDuplicationClass(data.cId, data.sId) == 1) {
-        console.log ("중복 수강신청 불가.");
-        res.redirect('/sugang');
-    } else if (await selectSql.checkZeroRemainClass(data.cId) == 1) {
-        console.log ("잔여 없음.");
-        res.redirect('/sugang');
+router.get('/user', async function (req, res) {
+    const userEmail = req.cookies.user_email;
+    const legInstance = await selectSql.getLegInstance(leg_number, airplane_id);
+    const seatReservation = await selectSql.getSeatReservation(date, user_id);
+    
+    if (userEmail) {
+        res.render('select', { 
+            user: req.cookies.user_email,
+            title: "legInstance",
+            title2: "seatReservation",
+            legInstance,
+            seatReservation
+        });
     } else {
-        await createSql.addClass(data);
-        res.redirect('/sugang');
+        res.render('/admin')
     }
 });
 
