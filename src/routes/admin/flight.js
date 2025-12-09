b51a5248a6a708402383a6ef24f7c42be36df1b4
박근채 <pgc0419@gmail.com>
2025-12-09 15:34:34 +0900

    feat flightLeg.js

--- src/routes/admin/flight.js
+++ src/routes/admin/flight.js
@@ -35,7 +35,7 @@ router.post('/admin/control/flight/update', async function (req, res) {
 
 router.post('/admin/control/flight/delete', async function (req, res) {
     const vars = req.body;
-    const id = vars.fare_code;
+    const id = vars.flight_number;
     await deleteSql.deleteFare(id);
     res.redirect('/admin/control/flight');
 });
