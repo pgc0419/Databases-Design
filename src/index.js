53bb1ffc03b6a054ccabf4e32de9bbd372577efe
박근채 <pgc0419@gmail.com>
2025-12-09 15:58:13 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import connectLiveReload from 'connect-livereload';
 import loginRouter from "./routes/login";
 import logoutRouter from './routes/logout';
 import selectRouter from "./routes/select";
+import airplaneRouter from "./routes/admin/airplane";
+import airplaneTypeRouter from "./routes/admin/airplaneType";
+import airportRouter from "./routes/admin/airport";
+import canLandRouter from "./routes/admin/canLand";
+import fareRouter from "./routes/admin/fare"
+import flightRouter from "./routes/admin/flight";;
+import flightLegRouter from "./routes/admin/flightLeg";
+import legInstanceRouter from "./routes/admin/legInstance";
 
 const PORT = 3000;
 
@@ -20,20 +28,32 @@ liveReloadServer.server.once("connection", () => {
 const app = express();
 
 app.use(connectLiveReload());
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/src')));
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
+app.use(
+    expressSession({
+        secret: "my key",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, 'public')));
-// css 경로 설정
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use("/", loginRouter);
 app.use("/logout", logoutRouter);
 app.use("/", selectRouter);
+app.use("/airplane", airplaneRouter);
+app.use("/airplaneType", airplaneTypeRouter);
+app.use("/airport", airportRouter);
+app.use("/canLand", canLandRouter);
+app.use("/fare", fareRouter);
+app.use("/flightLeg", flightLegRouter);
+app.use("/legInstance", legInstanceRouter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
